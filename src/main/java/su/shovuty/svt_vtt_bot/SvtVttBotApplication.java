f2de1f399914baea82d99ca5befcbe287aedceb0
Shovut <tbikba4ok@mail.ru>
2021-11-29 15:33:50 +0300

    init

---
+++ src/main/java/su/shovuty/svt_vtt_bot/SvtVttBotApplication.java
@@ -0,0 +1,13 @@
+package su.shovuty.svt_vtt_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SvtVttBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SvtVttBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/su/shovuty/svt_vtt_bot/SvtVttBotApplicationTests.java
@@ -0,0 +1,13 @@
+package su.shovuty.svt_vtt_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SvtVttBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
